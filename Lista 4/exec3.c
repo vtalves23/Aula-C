8599bd34cfca5454d110863ec25a892a97bf1236
vtalves23 <vitorantonio.felix@gmail.com>
2026-04-29 20:12:15 -0300

    exec3.c

---
+++ Lista 4/exec3.c
@@ -0,0 +1,13 @@
+// 3. Leia 5 numeros e calcule a soma apenas dos posivitos.
+#include <stdio.h>
+
+int main() {
+    int i, numero , soma = 0;
+    for (i = 1;i <= 5; i++) {
+  if (numero > 0) {
+      soma = soma + numero;
+}
+}
+printf("A soma foi de %d",numero);
+    return 0;
+}
